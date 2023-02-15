cff2aa910199e94a31d97aa487fe5cbacbba6aec
Atsushi Okui <okui@motionpicture.jp>
2023-02-15 06:18:16 +0000

    IPアドレスを表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
+import type { GetServerSidePropsResult } from 'next';
 import Alert from '@mui/material/Alert';
 import Layout from '../components/layouts/admin/layout';
 
-export default function Home() {
+type Props = {
+  ip: string | null;
+}
+
+export async function getServerSideProps(): Promise<GetServerSidePropsResult<Props>> {
+  const response = await fetch('https://ipapi.co/json/');
+  const data = await response.json();
+  const ip = data.ip;
+
+  return { props: { ip } };
+}
+
+export default function Home({ ip }: Props) {
   return (
     <Layout title={"Dashboard"}>
-      <Alert severity="success">This is a success alert — check it out!</Alert>
+      <Alert severity="success"><strong>{ip}</strong>からの接続に成功しました！</Alert>
     </Layout>
   );
 }
