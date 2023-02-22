58edb2a0ebc9085b1c4c60bb98501990db65e3e9
Atsushi Okui <okui@motionpicture.jp>
2023-02-22 21:52:13 +0900

    Fast Refreshのために環境変数を追加
    next.config.jsでの設定はやめる

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 5000,
-      aggregateTimeout: 300
-    }
-    return config;
-  }
 }
 
 module.exports = nextConfig
